f00b989033db0638a9aaee1ccfffb23be2778210
Steffen Baranowsky <BaraMGB@arcor.de>
2023-07-23 18:13:44 +0200

    init commit

---
+++ mappings.lua
@@ -0,0 +1,33 @@
+local M = {}
+-- In order to disable a default keymap, use
+M.disabled = {
+  n = {
+      -- ["<leader>e"] = ""
+  }
+}
+
+-- Your custom mappings
+M.abc = {
+  n = {
+    ["<C-n>"] = {"<cmd> Telescope <CR>", "Telescope"},
+    ["<C-s>"] = {":Telescope Files <CR>", "Telescope Files"},
+    ["<leader>e"] = {":NvimTreeToggle <CR>", "File Browser"},
+    ["<S-Down>"] = { "v gj", "visual mode"},
+    ["<leader>qn"] = {":qa!<cr>"},
+    ["<leader>h"] = { ":ClangdSwitchSourceHeader<cr>", "switch header source" },
+    ["<leader>t"] = {function() require("nvterm.terminal").new "horizontal"end, "New horizontal term",},
+  },
+  v = {
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
+    ["<S-Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
+    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
+    ["<S-Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
+  },
+
+  i = {
+     ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+    -- ...
+  }
+}
+
+return M
