93f9a896b859676b1b8dc7d3c67ddfa24e26565e
Steffen Baranowsky <BaraMGB@freenet.de>
2024-03-29 20:37:50 +0100

    Update refactor_function_cpp.lua
    remove comments

--- refactor_function_cpp.lua
+++ refactor_function_cpp.lua
@@ -2,40 +2,31 @@ local M = {}
 
 M.insertDoubleColonAtCursor = function()
     local current_buf = vim.api.nvim_get_current_buf()
-    -- Holen Sie die aktuelle Cursorposition (Zeile und Spalte)
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     local current_line = cursor_pos[1]
-    local current_col = cursor_pos[2] + 1 -- Lua ist 1-basiert, Vim ist 0-basiert
-
-    -- Holen Sie den Inhalt der aktuellen Zeile
-    local line_content = vim.api.nvim_buf_get_lines(current_buf, current_line - 1, current_line, false)[1]
-
-    -- Fügen Sie "::" an der Cursorposition ein
+    local current_col = cursor_pos[2] + 1 -- lua 1 based, vim is 0 based
+    local line_content = vim.api.nvim_buf_get_lines(current_buf, current_line - 1, current_line, false)[1]    
     local new_line_content = line_content:sub(1, current_col - 1) .. "::" .. line_content:sub(current_col)
 
-    -- Ersetzen Sie die aktuelle Zeile mit der neuen Zeile
+    -- swap line with new line
     vim.api.nvim_buf_set_lines(current_buf, current_line - 1, current_line, false, {new_line_content})
 end
+
 M.deleteLinesToRegister = function(start_line, end_line)
     local current_buf = vim.api.nvim_get_current_buf()
-
-    -- Abrufen der Zeilen im angegebenen Bereich
     local lines = vim.api.nvim_buf_get_lines(current_buf, start_line - 1, end_line, false)
-
-    -- Bearbeite jede Zeile, um "override" zu entfernen
+    
     for i, line in ipairs(lines) do
         lines[i] = line:gsub("override", "")
     end
-
-    -- Konvertiere die bearbeiteten Zeilen in einen String
+  
     local text_to_save = table.concat(lines, "\n")
 
-    -- Speichere den Text im Standardregister
     vim.fn.setreg('"', text_to_save)
 
-    -- Lösche die Zeilen aus dem Puffer
     vim.api.nvim_buf_set_lines(current_buf, start_line - 1, end_line, false, {})
 end
+
 M.moveFunctionToCpp = function()
     local current_buf = vim.api.nvim_get_current_buf()
     local current_line = vim.api.nvim_win_get_cursor(0)[1]
